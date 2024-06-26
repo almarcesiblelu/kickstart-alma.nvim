c874fdd94cc654dd3b97c5c3a0753839721b4fc4
shrimpmode <jsilva@uni.pe>
2024-06-25 20:10:50 -0500

    insert motions

--- lua/mappings/init.lua
+++ lua/mappings/init.lua
@@ -4,3 +4,14 @@ map.set('v', 'J', ":m '>+1<CR>gv=gv")
 map.set('v', 'K', ":m '<-2<CR>gv=gv")
 
 map.set({ 'n', 'i' }, '<C-s>', '<cmd> w <CR><ESC>', { desc = 'Save file' })
+
+map.set('i', '<C-l>', '<Right>', { noremap = true })
+map.set('i', '<C-h>', '<Left>', { noremap = true })
+map.set('i', '<C-j>', '<Down>', { noremap = true })
+map.set('i', '<C-k>', '<Up>', { noremap = true })
+map.set('i', '<C-f>', '<Right>', { noremap = true })
+map.set('i', '<C-b>', '<Left>', { noremap = true })
+-- C-e
+map.set('i', '<C-e>', '<End>', { noremap = true })
+-- C-a
+map.set('i', '<C-a>', '<Home>', { noremap = true })
